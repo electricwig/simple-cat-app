988553675285813ae565c9cc255e6695d4299782
Chris Killen <chriskillen@gmail.com>
2023-05-10 21:38:35 +0100

    Declare variables, get text element

--- javascript.js
+++ javascript.js
@@ -1,3 +1,7 @@
-function newCatFact() {
-    alert("This is working!");
+// declare our variables
+let newFact = "This is a random fact about a cat which will be displayed in the panel.";
+const factText = document.getElementById("fact");
+
+function displayNewCatFact() {
+    factText.innerHTML = newFact;
 }
